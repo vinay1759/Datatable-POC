1bb5dd0b8dff0f38cadb08f35372e6339460d075
Vinay Thakur <vinayt1759@gmail.com>
2023-02-22 20:05:36 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'datatablePOC';
+  public cols = [
+    { field: 'vin', header: 'Vin' },
+    { field: 'year', header: 'Year' },
+    { field: 'brand', header: 'Brand' },
+    { field: 'color', header: 'Color' }
+  ];
+  public cars = [
+    { "vin": "a165de", "year": 1992, "brand": "VW", "color": "Orange" },
+    { "vin": "j6w54qgh", "year": 1980, "brand": "Audi", "color": "Black" },
+    { "vin": "hrtwy34", "year": 2012, "brand": "Renault", "color": "Gray" },
+    { "vin": "jejtyj", "year": 2011, "brand": "BMW", "color": "Blue" },
+    { "vin": "g43gr", "year": 1995, "brand": "Mercedes", "color": "Orange" },
+  ]
 }
